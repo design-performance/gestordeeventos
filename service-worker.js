970022b7d64a1c5d3e54fde9287196cd14ebedef
design-performance <joandersantos@outlook.com>
2026-09-11 20:51:12 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gestor-eventos-v9';
+const CACHE = 'gestor-eventos-v10';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
